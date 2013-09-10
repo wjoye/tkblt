d2d5654b7141082d48dc0ea1b3a066f664d64426
joye <joye>
2013-09-10 19:15:01 +0000

    *** empty log message ***

--- src/bltGrPSOutput.C
+++ src/bltGrPSOutput.C
@@ -30,14 +30,6 @@
 #include <X11/Xutil.h>
 #include <X11/Xatom.h>
 
-#define TIME_WITH_SYS_TIME 0
-#define HAVE_SYS_TIME_H 1
-#define STDC_HEADERS 1
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_STDLIB_H 1
-#define HAVE_INTTYPES_H 1
-#define HAVE_STDINT_H 1
-#define HAVE_UNISTD_H 1
 #include <tkPort.h>
 #include <tkInt.h>
 #include <tk3d.h>
--- src/bltNsUtil.C
+++ src/bltNsUtil.C
@@ -26,14 +26,6 @@
  *	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#define TIME_WITH_SYS_TIME 0
-#define HAVE_SYS_TIME_H 1
-#define STDC_HEADERS 1
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_STDLIB_H 1
-#define HAVE_INTTYPES_H 1
-#define HAVE_STDINT_H 1
-#define HAVE_UNISTD_H 1
 #include <tclPort.h>
 #include <tclInt.h>
 
