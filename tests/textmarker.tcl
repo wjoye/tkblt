35c3d137f2666241c88c83016b9e6558ba5ae47b
joye <joye>
2014-03-19 19:19:22 +0000

    *** empty log message ***

--- tests/textmarker.tcl
+++ tests/textmarker.tcl
@@ -4,13 +4,12 @@ set w .line
 set graph [bltLineGraph $w]
 
 set mm [$graph marker create text tt -element data1 \
-	    -coords {1.5 100} -text {Text Marker} -font {24}]
-
+	    -x 1.5 -y 100 -text {Text Marker} -font {24}]
+return
 bltTest3 $graph marker $mm -anchor nw
 bltTest3 $graph marker $mm -background blue
 bltTest3 $graph marker $mm -bg red
 bltTest3 $graph marker $mm -bindtags {aa}
-bltTest3 $graph marker $mm -coords {1 50}
 bltTest3 $graph marker $mm -element data2
 bltTest3 $graph marker $mm -fg cyan
 bltTest3 $graph marker $mm -fill yellow
@@ -21,13 +20,13 @@ bltTest3 $graph marker $mm -hide yes
 bltTest3 $graph marker $mm -mapx x
 bltTest3 $graph marker $mm -mapy y
 bltTest3 $graph marker $mm -outline green
-bltTest3 $graph marker $mm -padx 10
-bltTest3 $graph marker $mm -pady 10
 bltTest3 $graph marker $mm -rotate 45
 bltTest3 $graph marker $mm -state disabled
 bltTest3 $graph marker $mm -text {Hello World}
 bltTest3 $graph marker $mm -under yes
+bltTest3 $graph marker $mm -x 1
 bltTest3 $graph marker $mm -xoffset 20
+bltTest3 $graph marker $mm -y 50
 bltTest3 $graph marker $mm -yoffset 20
 
 echo "done"
