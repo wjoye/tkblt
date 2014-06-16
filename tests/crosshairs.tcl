c5902b724705fea4d415314270264a392dcaa136
joye <joye>
2014-06-16 20:02:03 +0000

    *** empty log message ***

--- tests/crosshairs.tcl
+++ tests/crosshairs.tcl
@@ -8,12 +8,12 @@ $graph crosshairs configure -x 200 -y 200
 
 echo "Testing Crosshairs..."
 
-bltTest2 $graph crosshairs -color green $dops
-bltTest2 $graph crosshairs -dashes "8 3" $dops
-bltTest2 $graph crosshairs -hide yes $dops
-bltTest2 $graph crosshairs -linewidth 3 $dops
-bltTest2 $graph crosshairs -x 100 $dops
-bltTest2 $graph crosshairs -y 100 $dops
+bltTest2 $graph crosshairs -color green
+bltTest2 $graph crosshairs -dashes "8 3"
+bltTest2 $graph crosshairs -hide yes
+bltTest2 $graph crosshairs -linewidth 3
+bltTest2 $graph crosshairs -x 100
+bltTest2 $graph crosshairs -y 100
 
 bltCmd $graph crosshairs cget -color
 bltCmd $graph crosshairs configure
