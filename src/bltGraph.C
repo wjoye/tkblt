3fee4b82108b39335b300a3e86b097d72505f555
joye <joye>
2014-09-25 19:44:40 +0000

    *** empty log message ***

--- src/bltGraph.C
+++ src/bltGraph.C
@@ -377,9 +377,9 @@ int Graph::print(const char* ident, PSOutput* psPtr)
   float pica = 25.4 / 72 * 
     WidthOfScreen(Tk_Screen(tkwin_)) / 
     WidthMMOfScreen(Tk_Screen(tkwin_));
-  cerr << width_ << 'x' << height_ << endl;
-  //  width_ /= pica;
-  //  height_ /= pica;
+
+  width_ /= pica;
+  height_ /= pica;
 
   psPtr->computeBBox(width_, height_, pica);
   flags |= RESET;
