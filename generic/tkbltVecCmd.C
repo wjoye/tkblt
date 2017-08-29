wjoye/tkblt

commit 95101fc9f957cb4c3eee8b56b455e8b1f883e014

Interpret empty strings as NaN

generic/tkbltVecCmd.C
@@ -161,8 +161,17 @@ static int Blt_ExprDoubleFromObj(Tcl_Interp* interp, Tcl_Obj *objPtr,
   if (Tcl_GetDoubleFromObj((Tcl_Interp *)NULL, objPtr, valuePtr) == TCL_OK)
     return TCL_OK;
 
+  // Interpret the empty string as NaN
+  int length;
+  char *string;
+  string = Tcl_GetStringFromObj(objPtr, &length);
+  if (length == 0) {
+    *valuePtr = NAN;
+    return TCL_OK;
+  }
+
   // Then try to parse it as an expression.
-  if (Tcl_ExprDouble(interp, Tcl_GetString(objPtr), valuePtr) == TCL_OK)
+  if (Tcl_ExprDouble(interp, string, valuePtr) == TCL_OK)
     return TCL_OK;
 
   return TCL_ERROR;

commit 791beb6ff77644995abd53454ee96868245741ac

Add missing include defining NAN
Fixes problem introduced by 95101fc9f957cb4c3eee8b56b455e8b1f883e014
(Interpret empty strings as NaN) when compiling with MSVC.

generic/tkbltVecCmd.C
@@ -57,6 +57,7 @@
 #include "tkbltOp.h"
 #include "tkbltNsUtil.h"
 #include "tkbltSwitch.h"
+#include "tkbltInt.h"
 
 using namespace Blt;
 